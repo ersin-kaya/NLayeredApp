47196b4a6298123b1dcb01f73f810b9fc2732ed1
ersin <ersin-kaya@outlook.com.tr>
2025-05-10 02:30:23 +0300

    Add ISoftDeletable

---
+++ NLayeredApp.Core/ISoftDeletable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayeredApp.Core
+{
+    public interface ISoftDeletable
+    {
+        DateTimeOffset? DeletedAt { get; set; }
+        string? DeletedBy { get; set; }
+        bool IsDeleted { get; set; }
+    }
+}
