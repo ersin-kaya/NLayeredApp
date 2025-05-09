981015be00733d603637089618f65ec59f6a131a
ersin <ersin-kaya@outlook.com.tr>
2025-05-10 02:06:00 +0300

    Add AuditableEntity

---
+++ NLayeredApp.Core/AuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayeredApp.Core
+{
+    public abstract class AuditableEntity : BaseEntity
+    {
+        public DateTimeOffset CreatedAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTimeOffset? LastModifiedAt { get; set; }
+        public string? LastModifiedBy { get; set; }
+    }
+}
