e670523d86fde0fbb218fd0eb577a8051f34ee28
ersin <ersin-kaya@outlook.com.tr>
2025-05-21 18:22:27 +0300

    Add AuditableEntityConfiguration.cs

---
+++ NLayeredApp.Repository/Configurations/AuditableEntityConfiguration.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayeredApp.Core.Common;
+
+namespace NLayeredApp.Repository.Configurations;
+
+public abstract class AuditableEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : AuditableEntity
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.Property(c => c.CreatedBy)
+            .HasMaxLength(100);
+
+        builder.Property(c => c.LastModifiedBy)
+            .HasMaxLength(100);
+    }
+}
