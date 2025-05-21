04b9655998258406a7c794d1ce03e8dd52498720
ersin <ersin-kaya@outlook.com.tr>
2025-05-21 18:38:49 +0300

    Add CategoryConfiguration.cs

---
+++ NLayeredApp.Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayeredApp.Core.Entities;
+
+namespace NLayeredApp.Repository.Configurations;
+
+public class CategoryConfiguration : AuditableEntityConfiguration<Category>
+{
+    public override void Configure(EntityTypeBuilder<Category> builder)
+    {
+        base.Configure(builder);
+        
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(c => c.Description)
+            .HasMaxLength(1000);
+
+        builder.Property(c => c.IsActive)
+            .HasDefaultValue(true);
+        
+        builder.HasIndex(c => c.Name)
+            .IsUnique();
+    }
+}
