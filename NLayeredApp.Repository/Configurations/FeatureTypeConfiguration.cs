a173b18539d81e254d162c6f3eb84ca2e805b2d5
ersin <ersin-kaya@outlook.com.tr>
2025-05-21 18:43:42 +0300

    Add FeatureTypeConfiguration.cs

---
+++ NLayeredApp.Repository/Configurations/FeatureTypeConfiguration.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayeredApp.Core.Entities;
+
+namespace NLayeredApp.Repository.Configurations;
+
+public class FeatureTypeConfiguration : IEntityTypeConfiguration<FeatureType>
+{
+    public void Configure(EntityTypeBuilder<FeatureType> builder)
+    {
+        builder.Property(ft => ft.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.HasIndex(ft => ft.Name)
+            .IsUnique();
+    }
+}
