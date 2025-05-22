790b3a74eb57fe20940b5159b4fcd60531f73018
ersin <ersin-kaya@outlook.com.tr>
2025-05-23 01:10:43 +0300

    Add CategorySeed.cs

---
+++ NLayeredApp.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using NLayeredApp.Core.Entities;
+
+namespace NLayeredApp.Repository.Seeds;
+
+public static class CategorySeed
+{
+    public static void Apply(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            SeedHelpers.CreateCategory(
+                (int)SeedConstants.CategoryIds.Electronics,
+                "Electronics",
+                "Electronic products category"
+            ),
+            SeedHelpers.CreateCategory(
+                (int)SeedConstants.CategoryIds.Clothing,
+                "Clothing",
+                "Clothing products category"
+            ),
+            SeedHelpers.CreateCategory(
+                (int)SeedConstants.CategoryIds.HomeAndLiving,
+                "Home & Living",
+                "Home and living products"
+            )
+        );
+    }
+}
