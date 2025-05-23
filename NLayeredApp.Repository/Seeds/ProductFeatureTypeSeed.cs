6fd25ba6ae3440fc782f748367cad0ca3540ef80
ersin <ersin-kaya@outlook.com.tr>
2025-05-23 23:52:45 +0300

    Add ProductFeatureTypeSeed.cs

---
+++ NLayeredApp.Repository/Seeds/ProductFeatureTypeSeed.cs
@@ -0,0 +1,177 @@
+using Microsoft.EntityFrameworkCore;
+using NLayeredApp.Core.Entities;
+
+namespace NLayeredApp.Repository.Seeds;
+
+public static class ProductFeatureTypeSeed
+{
+    public static void Apply(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProductFeatureType>().HasData(
+            // Electronics
+            // Galaxy S25
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.GalaxyS25,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.Electronics.Colors.Silver
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.GalaxyS25,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Storage,
+                SeedConstants.FeatureIds.Electronics.StorageOptions.Gb256
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.GalaxyS25,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Memory,
+                SeedConstants.FeatureIds.Electronics.MemoryOptions.Gb16
+            ),
+            
+            // Galaxy S24
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.GalaxyS24,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.Electronics.Colors.White
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.GalaxyS24,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Storage,
+                SeedConstants.FeatureIds.Electronics.StorageOptions.Gb128
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.GalaxyS24,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Memory,
+                SeedConstants.FeatureIds.Electronics.MemoryOptions.Gb8
+            ),
+            
+            // Logitech Mouse
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.LogitechMouse,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.Electronics.Colors.Black
+            ),
+            
+            // Logitech Keyboard
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.LogitechKeyboard,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.Electronics.Colors.Black
+            ),
+            
+            // Logitech Headset
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.LogitechHeadset,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.Electronics.Colors.Black
+            ),
+            
+            // Logitech Gamepad
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.LogitechGamepad,
+                (int)SeedConstants.CategoryIds.Electronics,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.Electronics.Colors.Silver
+            ),
+            
+            // Clothing
+            // Polo Shirt
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.PoloShirt,
+                (int)SeedConstants.CategoryIds.Clothing,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.Clothing.Colors.Red
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.PoloShirt,
+                (int)SeedConstants.CategoryIds.Clothing,
+                (int)SeedConstants.FeatureTypeIds.Material,
+                SeedConstants.FeatureIds.Clothing.Materials.Cotton
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.PoloShirt,
+                (int)SeedConstants.CategoryIds.Clothing,
+                (int)SeedConstants.FeatureTypeIds.Size,
+                SeedConstants.FeatureIds.Clothing.Sizes.M
+            ),
+            
+            // Blue Denim Jean
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.BlueDenimJean,
+                (int)SeedConstants.CategoryIds.Clothing,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.Clothing.Colors.Blue
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.BlueDenimJean,
+                (int)SeedConstants.CategoryIds.Clothing,
+                (int)SeedConstants.FeatureTypeIds.Material,
+                SeedConstants.FeatureIds.Clothing.Materials.Denim
+            ),
+            
+            // Running Shoes
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.RunningShoes,
+                (int)SeedConstants.CategoryIds.Clothing,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.Clothing.Colors.Green
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.RunningShoes,
+                (int)SeedConstants.CategoryIds.Clothing,
+                (int)SeedConstants.FeatureTypeIds.Material,
+                SeedConstants.FeatureIds.Clothing.Materials.Polyester
+            ),
+            
+            // Home and Living
+            // Bookshelf
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.Bookshelf,
+                (int)SeedConstants.CategoryIds.HomeAndLiving,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.HomeAndLiving.Colors.Brown
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.Bookshelf,
+                (int)SeedConstants.CategoryIds.HomeAndLiving,
+                (int)SeedConstants.FeatureTypeIds.Material,
+                SeedConstants.FeatureIds.HomeAndLiving.Materials.Wood
+            ),
+            
+            // Dining Table Set
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.DiningTable,
+                (int)SeedConstants.CategoryIds.HomeAndLiving,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.HomeAndLiving.Colors.Gray
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.DiningTable,
+                (int)SeedConstants.CategoryIds.HomeAndLiving,
+                (int)SeedConstants.FeatureTypeIds.Material,
+                SeedConstants.FeatureIds.HomeAndLiving.Materials.Ceramic
+            ),
+            
+            // LED Floor Lamp
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.LedFloorLamp,
+                (int)SeedConstants.CategoryIds.HomeAndLiving,
+                (int)SeedConstants.FeatureTypeIds.Color,
+                SeedConstants.FeatureIds.HomeAndLiving.Colors.White
+            ),
+            SeedHelpers.CreateProductFeatureType(
+                SeedConstants.ProductIds.LedFloorLamp,
+                (int)SeedConstants.CategoryIds.HomeAndLiving,
+                (int)SeedConstants.FeatureTypeIds.Material,
+                SeedConstants.FeatureIds.HomeAndLiving.Materials.Metal
+            )
+        );
+    }
+}
