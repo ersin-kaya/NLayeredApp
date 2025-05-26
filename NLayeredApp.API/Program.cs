572583e4f5b9871c2cb83e82d12410bdc6dae109
ersin <ersin-kaya@outlook.com.tr>
2025-05-26 22:51:22 +0300

    Configure database and register repositories

--- NLayeredApp.API/Program.cs
+++ NLayeredApp.API/Program.cs
@@ -1,3 +1,11 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using NLayeredApp.Core.Repositories;
+using NLayeredApp.Core.UnitOfWork;
+using NLayeredApp.DataAccess;
+using NLayeredApp.DataAccess.Repositories;
+using NLayeredApp.DataAccess.UnitOfWork;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString, b =>
+        b.MigrationsAssembly(Assembly.GetAssembly(typeof(ApplicationDbContext)).GetName().Name)));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
