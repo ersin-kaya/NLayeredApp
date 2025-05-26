218e1c705c5e577e1580902f96198409c50b83c0
ersin <ersin-kaya@outlook.com.tr>
2025-05-27 00:34:25 +0300

    Add seed data calls and create SeedData migration

--- NLayeredApp.DataAccess/Migrations/20250526212835_SeedData.Designer.cs
+++ NLayeredApp.DataAccess/Migrations/20250526212835_SeedData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NLayeredApp.DataAccess;
 
@@ -11,9 +12,10 @@ using NLayeredApp.DataAccess;
 namespace NLayeredApp.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250526212835_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
