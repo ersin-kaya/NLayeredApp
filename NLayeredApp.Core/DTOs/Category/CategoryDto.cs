1443b4cc5ec1411b2d9b96b70f84cc00b75986e7
ersin <ersin-kaya@outlook.com.tr>
2025-05-28 20:56:29 +0300

    Add CategoryDto.cs

---
+++ NLayeredApp.Core/DTOs/Category/CategoryDto.cs
@@ -0,0 +1,23 @@
+namespace NLayeredApp.Core.DTOs.Category;
+
+public record CategoryDto
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string? Description { get; init; }
+    public bool IsActive { get; init; }
+    public DateTimeOffset CreatedAt { get; init; }
+    public DateTimeOffset? LastModifiedAt { get; init; }
+
+    public CategoryDto() { }
+
+    public CategoryDto(int id, string name, string? description, bool isActive, DateTimeOffset createdAt, DateTimeOffset? lastModifiedAt)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        IsActive = isActive;
+        CreatedAt = createdAt;
+        LastModifiedAt = lastModifiedAt;
+    }
+}
