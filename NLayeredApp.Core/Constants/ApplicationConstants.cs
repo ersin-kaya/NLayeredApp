25c0feda25bb7de523f4319e0c4908c21447dae9
ersin <ersin-kaya@outlook.com.tr>
2025-05-29 15:56:00 +0300

    Add ApplicationConstants.cs

---
+++ NLayeredApp.Core/Constants/ApplicationConstants.cs
@@ -0,0 +1,17 @@
+namespace NLayeredApp.Core.Constants;
+
+public static class ApplicationConstants
+{
+    // Pagination
+    public const int DefaultPageNumber = 1;
+    public const int DefaultPageSize = 10;
+    public const int MaxPageSize = 100;
+    
+    // String Lengths
+    public const int CreatedByMaxLength = 100;
+    public const int LastModifiedByMaxLength = 100;
+    public const int CategoryNameMaxLength = 100;
+    public const int CategoryDescriptionMaxLength = 500;
+    public const int ProductNameMaxLength = 200;
+    public const int ProductDescriptionMaxLength = 2000;
+}
