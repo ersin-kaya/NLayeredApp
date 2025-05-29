a353e27540ae9f80b99b4518ed608ca3735f3975
ersin <ersin-kaya@outlook.com.tr>
2025-05-29 16:18:48 +0300

    Add new constants to ApplicationConstants

--- NLayeredApp.Core/Constants/ApplicationConstants.cs
+++ NLayeredApp.Core/Constants/ApplicationConstants.cs
@@ -10,8 +10,23 @@ public static class ApplicationConstants
     // String Lengths
     public const int CreatedByMaxLength = 100;
     public const int LastModifiedByMaxLength = 100;
+    public const int DeletedByMaxLength = 100;
+    
     public const int CategoryNameMaxLength = 100;
     public const int CategoryDescriptionMaxLength = 500;
+    public const bool CategoryIsActive = true;
+    
     public const int ProductNameMaxLength = 200;
     public const int ProductDescriptionMaxLength = 2000;
+    public const int ProductPricePrecision = 18;
+    public const int ProductPriceScale = 2;
+    public const bool ProductIsActive = true;
+    public const bool ProductIsDeleted = false;
+    
+    public const int CategoryFeatureTypeLabelMaxLength = 100;
+    public const bool CategoryFeatureTypeIsRequired = false;
+
+    public const int FeatureValueMaxLength = 100;
+
+    public const int FeatureTypeNameMaxLength = 100;
 }
