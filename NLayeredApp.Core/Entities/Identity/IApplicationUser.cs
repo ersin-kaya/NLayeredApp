29338c49d082e56db41b10c52642a1286b322f57
ersin <ersin-kaya@outlook.com.tr>
2025-06-11 22:59:34 +0300

    Add IApplicationUser to represent extended user properties

---
+++ NLayeredApp.Core/Entities/Identity/IApplicationUser.cs
@@ -0,0 +1,11 @@
+using NLayeredApp.Core.Common;
+
+namespace NLayeredApp.Core.Entities.Identity;
+
+public interface IApplicationUser : IEntity, IAuditable, IActivatable, ISoftDeletable
+{
+    string UserName { get; set; }
+    string Email { get; set; }
+    string FirstName { get; set; }
+    string LastName { get; set; }
+}
