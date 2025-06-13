b9e7fb4fd0b1385ed9efe16b3bf2479de4ed0671
ersin <ersin-kaya@outlook.com.tr>
2025-06-13 22:54:24 +0300

    Update ExpiresAt type to DateTimeOffset in LoginResponse

--- NLayeredApp.Core/DTOs/Auth/Responses/LoginResponse.cs
+++ NLayeredApp.Core/DTOs/Auth/Responses/LoginResponse.cs
@@ -5,7 +5,7 @@ public class LoginResponse
     public bool IsSuccess { get; set; }
     public string? Token { get; set; }
     public string? RefreshToken { get; set; }
-    public DateTime? ExpiresAt { get; set; }
+    public DateTimeOffset? ExpiresAt { get; set; }
     public UserDto? User { get; set; }
     public List<string> Errors { get; set; } = new();
 }
