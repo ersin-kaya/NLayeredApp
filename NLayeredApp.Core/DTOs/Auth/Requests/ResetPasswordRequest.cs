de1b67fafda010a604c05518b544de8841d43550
ersin <ersin-kaya@outlook.com.tr>
2025-06-13 22:56:22 +0300

    Add ResetPasswordRequest

---
+++ NLayeredApp.Core/DTOs/Auth/Requests/ResetPasswordRequest.cs
@@ -0,0 +1,8 @@
+namespace NLayeredApp.Core.DTOs.Auth.Requests;
+
+public class ResetPasswordRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+}
