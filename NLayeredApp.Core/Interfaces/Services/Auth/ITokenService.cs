7b69afc60b60929218c4f168fc4a04953602788a
ersin <ersin-kaya@outlook.com.tr>
2025-06-13 23:29:14 +0300

    Add ITokenService

---
+++ NLayeredApp.Core/Interfaces/Services/Auth/ITokenService.cs
@@ -0,0 +1,14 @@
+using NLayeredApp.Core.DTOs.Auth.Responses;
+using NLayeredApp.Core.Entities.Identity;
+
+namespace NLayeredApp.Core.Interfaces.Services.Auth;
+
+public interface ITokenService
+{
+    Task<RefreshTokenResponse> GenerateTokenAsync(IApplicationUser user);
+    Task<string?> ValidateTokenAsync(string token);
+    Task<RefreshToken> GenerateRefreshTokenAsync(int userId);
+    Task<bool> ValidateRefreshTokenAsync(string refreshToken);
+    Task<bool> RevokeTokenAsync(string refreshToken);
+    Task<bool> RevokeAllUserTokensAsync(int userId);
+}
