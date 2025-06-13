f7cb196cee52ab39bf6f9424c50f8a695deb5233
ersin <ersin-kaya@outlook.com.tr>
2025-06-13 23:16:56 +0300

    Add RoleDto

---
+++ NLayeredApp.Core/DTOs/Identity/RoleDto.cs
@@ -0,0 +1,10 @@
+namespace NLayeredApp.Core.DTOs.Identity;
+
+public class RoleDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public bool IsActive { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+}
