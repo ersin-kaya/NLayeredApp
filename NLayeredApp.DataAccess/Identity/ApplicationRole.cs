7fa8cc86befbc55a1b1d0c2f0b2377e18205bce7
ersin <ersin-kaya@outlook.com.tr>
2025-06-15 00:43:52 +0300

    Add ApplicationRole and update constants in ApplicationUser

--- NLayeredApp.DataAccess/Identity/ApplicationRole.cs
+++ NLayeredApp.DataAccess/Identity/ApplicationRole.cs
@@ -4,19 +4,18 @@ using NLayeredApp.Core.Entities.Identity;
 
 namespace NLayeredApp.DataAccess.Identity;
 
-public class ApplicationUser : IdentityUser<int>, IApplicationUser
+public class ApplicationRole : IdentityRole<int>, IApplicationRole
 {
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-
-    public bool IsActive { get; set; } = ApplicationConstants.Identity.User.IsActiveDefault;
+    public string? Description { get; set; }
     
     public DateTimeOffset CreatedAt { get; set; }
     public string? CreatedBy { get; set; }
     public DateTimeOffset? LastModifiedAt { get; set; }
     public string? LastModifiedBy { get; set; }
     
+    public bool IsActive { get; set; } = ApplicationConstants.Identity.Role.IsActiveDefault;
+    
     public DateTimeOffset? DeletedAt { get; set; }
     public string? DeletedBy { get; set; }
-    public bool IsDeleted { get; set; } = ApplicationConstants.Identity.User.IsDeletedDefault;
+    public bool IsDeleted { get; set; } = ApplicationConstants.Identity.Role.IsDeletedDefault;
 }
